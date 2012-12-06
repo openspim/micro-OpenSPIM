30e8e29ca786e8a0c751d60c792cf0e697ec9319
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2012-12-06 16:49:07 -0600

    Added support for our Cube in Madison.
    We'll talk about getting the Dresden setup working ASAP (presumably
    Monday). At the moment, the power range is falling back to defaults 0.05mW
    - 20mW, but I'm working on that.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- spim/SPIMAcquisition.java
+++ spim/SPIMAcquisition.java
@@ -277,6 +277,8 @@ public class SPIMAcquisition implements MMPlugin, MouseMotionListener, KeyListen
 			}
 			else if (driver.equals("Picard XY Stage"))
 				xyStageLabel = label;
+			else if (driver.equals("CoherentCube"))
+				laserLabel = label;
 			// testing
 			else if (driver.equals("DStage")) {
 				if (label.equals("DStage"))
@@ -773,11 +775,26 @@ public class SPIMAcquisition implements MMPlugin, MouseMotionListener, KeyListen
 		right.setLayout(new BoxLayout(right, BoxLayout.PAGE_AXIS));
 		right.setBorder(BorderFactory.createTitledBorder("Acquisition"));
 
+		int minlp = 5;
+		int deflp = 500;
+		int maxlp = 2000;
+		try {
+			minlp = (int)(Float.parseFloat(mmc.getProperty(laserLabel, "Minimum Laser Power")) * 100.0f);
+			deflp = (int)(Float.parseFloat(mmc.getProperty(laserLabel, "PowerSetpoint")) * 100.0f);
+			maxlp = (int)(Float.parseFloat(mmc.getProperty(laserLabel, "Maximum Laser Power")) * 100.0f);
+		} catch (Throwable e) {
+			ReportingUtils.logError(e);
+		}
+		
 		// TODO: find out correct values
-		laserSlider = new MotorSlider(0, 1000, 1000, "laser.power") {
+		laserSlider = new MotorSlider(minlp, maxlp, deflp, "laser.power") {
 			@Override
 			public void valueChanged(int value) {
-				// TODO
+				try {
+					mmc.setProperty(laserLabel, "PowerSetpoint", (float)value / 100.0f);
+				} catch (Exception e) {
+					ReportingUtils.logError(e);
+				}
 			}
 		};
 
@@ -873,7 +890,7 @@ public class SPIMAcquisition implements MMPlugin, MouseMotionListener, KeyListen
 			};
 		});
 
-		addLine(right, Justification.RIGHT, "Laser power:", laserPower, "exposure:", exposure);
+		addLine(right, Justification.RIGHT, "Laser power (e-5 W):", laserPower, "Exposure (ms):", exposure);
 		addLine(right, Justification.STRETCH, "Laser:", laserSlider);
 		addLine(right, Justification.STRETCH, "Exposure:", exposureSlider);
 		addLine(right, Justification.RIGHT, speedControl, "Z settle time (ms):", settleTime, continuousCheckbox, antiDriftCheckbox, liveCheckbox, registrationCheckbox);
@@ -1180,7 +1197,11 @@ public class SPIMAcquisition implements MMPlugin, MouseMotionListener, KeyListen
 		}
 
 		if (laserLabel != null) {
-			// TODO: get current laser power
+			// I know I'm supposed to use the readback property for power, but
+			// this updates the control's displayed value, which should match
+			// the desired power setting...
+			double psp = Double.parseDouble(mmc.getProperty(laserLabel, "PowerSetpoint"));
+			laserSlider.updateValueQuietly((int)(psp * 100.0f));
 		}
 
 		if (cameraLabel != null) {
