c0c9609bb0197e13be83b839794100a3866eb2ef
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2013-01-18 16:19:50 -0600

    Further updates to anti-drift
    Refactored here and there, fixed some file saving issues.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- spim/progacq/ProjDiffAntiDrift.java
+++ spim/progacq/ProjDiffAntiDrift.java
@@ -106,23 +106,60 @@ public class ProjDiffAntiDrift implements AntiDrift {
 			new ImagePlus("ZY", zy).show();
 		}
 
-		public ColorProcessor getDiff(final Projections other, double scale, double zratio, int dx, int dy, int dz) {
+		public void writeDiff(final Projections other, double zratio, Vector3D d, Vector3D c, File file) {
+			final ColorProcessor cp = getDiff(other, 1.0D, zratio, d, c);
+			ij.IJ.save(new ImagePlus("diff", cp), file.getAbsolutePath());
+		}
+
+		public ColorProcessor getDiff(final Projections other, double scale, double zratio, Vector3D dv, Vector3D center) {
 			final int w = (int) (xy.getWidth()*scale);
 			final int h = (int) (xy.getHeight()*scale);
 			final int d = (int) (xz.getHeight()*zratio*scale);
 
+			return makePanel(
+					drawCrosshair(getDiff(xy, other.xy, dv.getX(), dv.getY()), w, h, center.getX(), center.getY()),
+					drawCrosshair(getDiff(xz, other.xz, dv.getX(), dv.getZ()), w, d, center.getX(), center.getZ()*zratio),
+					drawCrosshair(getDiff(zy, other.zy, dv.getZ(), dv.getY()), d, h, center.getZ()*zratio, center.getY())
+					);
+		}
+
+		private ColorProcessor drawCrosshair(ColorProcessor on, int fw, int fh, double cx, double cy) {
+			int centerX = (int) (cx * fw / on.getWidth() + 0.5);
+			int centerY = (int) (cy * fh / on.getHeight() + 0.5);
+
+			on = (ColorProcessor) on.resize(fw, fh);
+
+			int[] pixels = (int[]) on.getPixels();
+
+			if(centerX >= 0 && centerX < fw)
+				for(int y = 0; y < fh; ++y)
+					pixels[centerX + y * fw] ^= 0x00FFFFFF;
+
+			if(centerY >= 0 && centerY < fh)
+				for(int x = 0; x < fw; ++x)
+					pixels[x + centerY * fw] ^= 0x00FFFFFF;
+
+			return on;
+		}
+
+		private static ColorProcessor makePanel(final ImageProcessor xy, final ImageProcessor xz, final ImageProcessor zy) {
+			final int w = xy.getWidth();
+			final int h = xy.getHeight();
+			final int d = xz.getHeight();
+			assert w == xz.getWidth() && h == zy.getHeight() && d == zy.getWidth();
+
 			final ColorProcessor result = new ColorProcessor(w + 1 + d, h + 1 + d);
 			final ColorBlitter blitter = new ColorBlitter(result);
-			blitter.copyBits(getDiff(xy, other.xy, dx, dy).resize(w, h), 0, 0, Blitter.COPY);
-			blitter.copyBits(getDiff(xz, other.xz, dx, dz).resize(w, d), 0, h, Blitter.COPY);
-			blitter.copyBits(getDiff(zy, other.zy, dz, dy).resize(d, h), w, 0, Blitter.COPY);
+			blitter.copyBits(xy, 0, 0, Blitter.COPY);
+			blitter.copyBits(xz, 0, h, Blitter.COPY);
+			blitter.copyBits(zy, w, 0, Blitter.COPY);
 			result.setColor(java.awt.Color.YELLOW);
 			result.drawLine(0, h, w+d, h);
 			result.drawLine(w, 0, w, h+d);
 			return result;
 		}
 
-		private static ColorProcessor getDiff(final FloatProcessor fp1, final FloatProcessor fp2, int dx, int dy) {
+		private static ColorProcessor getDiff(final FloatProcessor fp1, final FloatProcessor fp2, double dx, double dy) {
 			fp1.findMinAndMax();
 			final double min1 = fp1.getMin();
 			final double max1 = fp1.getMax();
@@ -142,7 +179,7 @@ public class ProjDiffAntiDrift implements AntiDrift {
 				for (int x = 0; x < w1; x++) {
 					int value1 = normalize(fp1.getf(x, y), min1, max1);
 					int value2 = x + dx < 0 || x + dx >= w2 || y + dy < 0 || y + dy >= h2 ?
-						0 : normalize(fp2.getf(x + dx, y + dy), min2, max2);
+						0 : normalize(fp2.getf(x + (int)dx, y + (int)dy), min2, max2);
 					pixels[x + w1 * y] = (value1 << 16) | (value2 << 8) | value1;
 				}
 			}
@@ -179,7 +216,6 @@ public class ProjDiffAntiDrift implements AntiDrift {
 			if(xyc == null || xzc == null || zyc == null)
 				return Vector3D.ZERO;
 
-			// TODO: Figure out why this seems to be too little...
 			return new Vector3D(xyc[0] + xzc[0], xyc[1] + zyc[1],
 				xzc[1] + zyc[0]).scalarMultiply(0.5D);
 		}
@@ -199,62 +235,48 @@ public class ProjDiffAntiDrift implements AntiDrift {
 		public static Projections get(final ImagePlus imp) {
 			final ImageStack stack = imp.getStack();
 			final Projections p = new Projections();
-			for (int i = 1; i <= stack.getSize(); i++) {
+
+			for (int i = 1; i <= stack.getSize(); i++)
 				p.addXYSlice(stack.getProcessor(i));
-			}
+
 			return p;
+		}
+
+		public Vector3D getCenter() {
+			return new Vector3D(xy.getWidth() / 2.0, xy.getHeight() / 2.0, xz.getHeight() / 2.0);
 		}
 	}
 
 	public interface Callback {
-		void offset(int dx, int dy, int dz);
+		void offset(Vector3D offs);
 	}
 
+	@SuppressWarnings("serial")
 	private static class AdjusterGUI extends JFrame implements KeyListener {
 		private Projections before, after;
 		private Callback callback;
-		private int dx, dy, dz;
+		private Vector3D offset;
 		private Dimension preferredImageSize;
 		private Image diff;
 		private JPanel panel;
 		private double scale, zratio;
-		private File outputFile;
+		private Vector3D center;
 
-		public AdjusterGUI(Vector3D loc, double theta, long tp, double izratio,
-				File saveDir, final Projections first, final Projections latest,
-				final Callback callback) {
+		public AdjusterGUI(final Vector3D loc, final double theta, final long tp,
+				final double izratio, final Projections first, final Projections latest,
+				final Vector3D initial, Vector3D center, final Callback callback) {
 			System.gc();
 
 			this.before = first;
 			this.after = latest;
 			this.callback = callback;
+			this.offset = initial;
+			this.center = center;
 
-			if(saveDir != null) {
-				String xyz = String.format("XYZ%.2fx%.2fx%.2f_Theta%.2f", loc.getX(), loc.getY(), loc.getZ(), theta);
-				String fn = String.format("diff_TL%02d", tp);
-				outputFile = new File(new File(saveDir, xyz), fn);
-
-				if(!outputFile.getParentFile().exists() && !outputFile.getParentFile().mkdirs()) {
-					ij.IJ.log("Couldn't create output directory for diff. (Continuing.)");
-					outputFile = null;
-				}
-			} else {
-				outputFile = null;
-			}
-
-			scale = getToolkit().getScreenSize().width / (after.largestDimension() * 2) * 0.9;
+			scale = getToolkit().getScreenSize().height / (after.largestDimension() * 2) * 0.9;
 			zratio = izratio;
-			writeDiff("initial");
 
-			Vector3D offs = latest.correlateAndAverage(first);
-			dx = (int) offs.getX();
-			dy = (int) offs.getY();
-			dz = (int) offs.getZ();
 			updateDiff();
-
-			writeDiff("suggested");
-
-			ij.IJ.log("Suggested offset: " + offs.toString());
 
 			panel = new JPanel() {
 				@Override
@@ -269,44 +291,39 @@ public class ProjDiffAntiDrift implements AntiDrift {
 			pack();
 			setTitle(String.format("xyz: %.2f x %.2f x %.2f, theta: %.2f, timepoint %02d", loc.getX(), loc.getY(), loc.getZ(), theta, tp));
 		}
-
+		
 		public void keyPressed(final KeyEvent e) {
 			switch (e.getKeyCode()) {
 				case KeyEvent.VK_ENTER:
-					if(outputFile != null)
-						writeDiff("final_saved");
 					dispose();
-					callback.offset(dx, dy, dz);
+					callback.offset(offset);
 					break;
 				case KeyEvent.VK_ESCAPE:
-					if(outputFile != null) {
-						dx = dy = dz = 0;
-						writeDiff("final_aborted");
-					}
 					dispose();
+					callback.offset(Vector3D.ZERO);
 					break;
 				case KeyEvent.VK_UP:
-					dy += (e.isShiftDown() ? 10 : 1);
+					offset = offset.add(new Vector3D(0, (e.isShiftDown() ? 10 : 1), 0));
 					updateDiff();
 					break;
 				case KeyEvent.VK_DOWN:
-					dy -= (e.isShiftDown() ? 10 : 1);
+					offset = offset.subtract(new Vector3D(0, (e.isShiftDown() ? 10 : 1), 0));
 					updateDiff();
 					break;
 				case KeyEvent.VK_LEFT:
-					dx += (e.isShiftDown() ? 10 : 1);
+					offset = offset.add(new Vector3D((e.isShiftDown() ? 10 : 1), 0, 0));
 					updateDiff();
 					break;
 				case KeyEvent.VK_RIGHT:
-					dx -= (e.isShiftDown() ? 10 : 1);
+					offset = offset.subtract(new Vector3D((e.isShiftDown() ? 10 : 1), 0, 0));
 					updateDiff();
 					break;
 				case KeyEvent.VK_PAGE_UP:
-					dz -= (e.isShiftDown() ? 10 : 1);
+					offset = offset.subtract(new Vector3D(0, 0, (e.isShiftDown() ? 10 : 1)));
 					updateDiff();
 					break;
 				case KeyEvent.VK_PAGE_DOWN:
-					dz += (e.isShiftDown() ? 10 : 1);
+					offset = offset.add(new Vector3D(0, 0, (e.isShiftDown() ? 10 : 1)));
 					updateDiff();
 					break;
 				case KeyEvent.VK_MINUS:
@@ -331,7 +348,7 @@ public class ProjDiffAntiDrift implements AntiDrift {
 		}
 
 		private void updateDiff() {
-			final ColorProcessor cp = before.getDiff(after, scale, zratio, dx, dy, dz);
+			final ColorProcessor cp = before.getDiff(after, scale, zratio, offset, center);
 			preferredImageSize = new Dimension(cp.getWidth(), cp.getHeight());
 			if(diff != null) {
 				diff.flush();
@@ -345,35 +362,29 @@ public class ProjDiffAntiDrift implements AntiDrift {
 			}
 		}
 
-		private void writeDiff(String suffix) {
-			ImagePlus imp = new ImagePlus(getTitle(), before.getDiff(after, scale, zratio, dx, dy, dz));
-			ij.IJ.save(imp, outputFile.getAbsolutePath() + "_" + suffix + ".tiff");
-			imp = null; // TODO: kill this; temporarily make sure we return memory to initial state with checkMem.
-		}
-
 		@Override
 		public void dispose() {
-			super.dispose();
 			diff.flush();
 			diff = null;
 			before = after = null;
+			super.dispose();
 		}
 	}
 
-	private Vector3D offset;
+	private Vector3D offset, lastCorrection;
 	private Projections first, latest;
 	private AdjusterGUI gui;
 
 	private long tp;
-	private Vector3D loc;
+	private Vector3D loc, center;
 	private double theta;
 	private double zstep;
 
-	private File saveDir;
+	private File outputDir;
 	private double zratio;
 
 	public ProjDiffAntiDrift(File outDir, AcqRow r) {
-		offset = Vector3D.ZERO;
+		offset = lastCorrection = Vector3D.ZERO;
 		first = null;
 
 		loc = new Vector3D(r.getX(), r.getY(), r.getStartPosition());
@@ -382,8 +393,16 @@ public class ProjDiffAntiDrift implements AntiDrift {
 		zstep = r.getStepSize();
 		zratio = zstep*2.3; // TODO: This is hard-coded.
 
-		if(outDir != null)
-			saveDir = new File(outDir, "diffs");
+		if(outDir != null) {
+			String xyz = String.format("XYZ%.2fx%.2fx%.2f_Theta%.2f", loc.getX(), loc.getY(), loc.getZ(), theta);
+			File saveDir = new File(new File(outDir, "diffs"), xyz);
+
+			if(!saveDir.exists() && !saveDir.mkdirs()) {
+				ij.IJ.log("Couldn't create output directory " + saveDir.getAbsolutePath());
+			} else {
+				outputDir = outDir;
+			}
+		}
 	}
 
 	@Override
@@ -394,9 +413,7 @@ public class ProjDiffAntiDrift implements AntiDrift {
 	@Override
 	public void startNewStack() {
 		if(gui != null && gui.isVisible()) {
-			if(gui.outputFile != null)
-				gui.writeDiff("final_slid");
-			gui.callback.offset(gui.dx, gui.dy, gui.dz);
+			gui.callback.offset(gui.offset);
 
 			gui.setVisible(false);
 			gui.dispose();
@@ -415,16 +432,38 @@ public class ProjDiffAntiDrift implements AntiDrift {
 	public void finishStack() {
 		finishStack(first == null);
 	}
+	
+	private File getOutFile(String tag) {
+		if(outputDir != null)
+			return new File(outputDir, String.format("diff_TL%02d_%s.tiff", tp, tag));
+		else
+			return null;
+	}
 
 	@Override
 	public void finishStack(boolean initial) {
-		if(first == null)
+		if(initial)
 			first = latest;
+		
+		final Vector3D center = lastCorrection.add(latest.getCenter());
 
-		gui = new AdjusterGUI(loc, theta, tp, zratio, saveDir, first, latest, new Callback() {
+		if(outputDir != null)
+			latest.writeDiff(first, zratio, lastCorrection, center, getOutFile("initial"));
+
+		Vector3D init = latest.correlateAndAverage(first);
+
+		if(outputDir != null)
+			latest.writeDiff(first, zratio, init, center, getOutFile("suggested"));
+
+		gui = new AdjusterGUI(loc, theta, tp, zratio, first, latest, init, center, new Callback() {
 			@Override
-			public void offset(int dx, int dy, int dz) {
-				offset = offset.add(new Vector3D(-dx, -dy, -dz*zstep));
+			public void offset(Vector3D offs) {
+				offs = offs.add(lastCorrection);
+				lastCorrection = offs;
+				offset = offset.add(new Vector3D(-offs.getX(), -offs.getY(), -offs.getZ()*zstep));
+
+				if(outputDir != null)
+					latest.writeDiff(first, zratio, offs, center, getOutFile("final"));
 			}
 		});
 		gui.setVisible(true);
