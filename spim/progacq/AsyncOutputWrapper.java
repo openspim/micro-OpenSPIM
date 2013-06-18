23404137df659c99c2b9fcc8bff6df94a0ddd190
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2013-06-18 16:09:03 -0500

    Async output wrapper: Fix concurrency issue
    Some misused synchronize blocks were causing various issues -- see bug
    tracker 114 and 120 for details -- I've replaced them as appropriate.
    Repetitive testing hasn't caused their reappearance, but I'll keep my ear
    to the ground on this one.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- spim/progacq/AsyncOutputWrapper.java
+++ spim/progacq/AsyncOutputWrapper.java
@@ -4,14 +4,17 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import java.lang.InterruptedException;
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.nio.channels.ClosedByInterruptException;
+
+import org.micromanager.utils.ReportingUtils;
 
 import ij.ImagePlus;
 import ij.process.ImageProcessor;
 
 import mmcorej.TaggedImage;
 
-public class AsyncOutputWrapper implements AcqOutputHandler {
+public class AsyncOutputWrapper implements AcqOutputHandler, UncaughtExceptionHandler {
 	private class IPC {
 		public IPC(ImageProcessor ip, double x, double y, double z, double t, double dt) {
 			this.ip = ip;
@@ -29,6 +32,7 @@ public class AsyncOutputWrapper implements AcqOutputHandler {
 	private AcqOutputHandler handler;
 	private Thread writerThread;
 	private BlockingQueue<IPC> queue;
+	private Exception rethrow;
 
 	private Runnable writerOp = new Runnable() {
 		@Override
@@ -39,7 +43,7 @@ public class AsyncOutputWrapper implements AcqOutputHandler {
 				if(!(e instanceof InterruptedException) &&
 				   !(e instanceof ClosedByInterruptException) ) {
 					ij.IJ.log("Async writer failed!");
-					ij.IJ.handleException(e);
+					throw new RuntimeException(e);
 				} else if(e instanceof ClosedByInterruptException) {
 					ij.IJ.log("Warning: asynchronous writer may have been cancelled before completing. (" + queue.size() + ")");
 				}
@@ -52,24 +56,40 @@ public class AsyncOutputWrapper implements AcqOutputHandler {
 		queue = new LinkedBlockingQueue<IPC>((int) cap);
 		writerThread = new Thread(writerOp, "Async Output Handler Thread");
 		writerThread.setPriority(Thread.MIN_PRIORITY);
+		writerThread.setUncaughtExceptionHandler(this);
 
+		rethrow = null;
 		writerThread.start();
 	}
 
 	@Override
 	public ImagePlus getImagePlus() throws Exception {
+		if(rethrow != null)
+			throw rethrow;
+
 		writeAll();
-		return handler.getImagePlus();
+
+		synchronized(handler) {
+			return handler.getImagePlus();
+		}
 	}
 
 	@Override
 	public void beginStack(int axis) throws Exception {
-		handler.beginStack(axis);
+		if(rethrow != null)
+			throw rethrow;
+
+		synchronized(handler) {
+			handler.beginStack(axis);
+		}
 	}
 
 	@Override
 	public void processSlice(ImageProcessor ip, double X, double Y, double Z,
 			double theta, double deltaT) throws Exception {
+		if(rethrow != null)
+			throw rethrow;
+
 		IPC store = new IPC(ip, X, Y, Z, theta, deltaT);
 		if(!queue.offer(store)) {
 			writeNext();
@@ -79,29 +99,67 @@ public class AsyncOutputWrapper implements AcqOutputHandler {
 
 	@Override
 	public void finalizeStack(int depth) throws Exception {
+		if(rethrow != null)
+			throw rethrow;
+
 		writeAll();
-		handler.finalizeStack(depth);
+
+		synchronized(handler) {
+			handler.finalizeStack(depth);
+		}
 	}
 
 	@Override
 	public void finalizeAcquisition() throws Exception {
+		if(rethrow != null)
+			throw rethrow;
+
 		writeAll();
 		writerThread.interrupt();
 		writerThread.join();
 
-		handler.finalizeAcquisition();
+		synchronized(handler) {
+			handler.finalizeAcquisition();
+		}
 	}
 
 	private void writeNext() throws Exception {
-		synchronized(queue) {
-			IPC write = queue.poll();
-			if(write != null)
+		if(rethrow != null)
+			throw rethrow;
+
+		IPC write = queue.poll();
+		if(write != null)
+		{
+			synchronized(handler) {
 				handler.processSlice(write.ip, write.x, write.y, write.z, write.t, write.dt);
+			}
 		}
 	}
-	
+
 	private void writeAll() throws Exception {
+		if(rethrow != null)
+		{
+			if(Thread.currentThread() != writerThread)
+				throw rethrow;
+			else
+				return;
+		};
+
 		while(!queue.isEmpty())
 			writeNext();
+	}
+
+	@Override
+	public void uncaughtException(Thread thread, Throwable exc) {
+		if(thread != writerThread)
+			throw new Error("Unexpected exception mis-caught.", exc);
+
+		if(!(exc instanceof Exception))
+		{
+			ReportingUtils.logException("Non-exception throwable " + exc.toString() + " caught from writer thread. Wrapping.", exc);
+			exc = new Exception("Wrapped throwable; see core log for details: " + exc.getMessage(), exc);
+		};
+
+		rethrow = (Exception)exc;
 	}
 }
