444c54e00a04d9e441dbe5278053a892a453daa1
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2013-08-08 16:11:10 -0500

    Completely reworked device manager
    The new model basically builds an object-oriented layer on top of MM's
    JNI. It's fast enough for us, but there are a few weird kinks to work out
    yet. Especially for the Picard motors, which have a stepping precision of
    1.5um, not everything works like we might expect (in particular, the
    rotational axis calibration is now broken -- or has it been for a while?).
    
    The biggest thing left to do is rewrite the motor sliders to use motor
    steps behind the scenes: the labels will read in um (or degrees, for the
    twister), but moving the motors by 1 or 10 um (i.e. by clicking the
    buttons with or without shift) will move by however many *steps* bring the
    motor closest to that delta.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

---
+++ src/main/java/spim/setup/Device.java
@@ -0,0 +1,172 @@
+package spim.setup;
+
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.micromanager.utils.ReportingUtils;
+
+import spim.setup.SPIMSetup.SPIMDevice;
+
+import mmcorej.CMMCore;
+import mmcorej.DeviceType;
+
+public abstract class Device {
+	protected CMMCore core;
+	protected String label;
+
+	/**
+	 * Basic constructor for a device.
+	 * 
+	 * @param core The CMMCore instance to which this device is attached.
+	 * @param label The MM label of this device.
+	 */
+	public Device(CMMCore core, String label) {
+		this.core = core;
+		this.label = label;
+	}
+
+	/**
+	 * Returns the device's internal name. This is the string passed to the
+	 * device adapter when creating a particular instance of the same ilk as
+	 * this device; that is, this name is more like a class name than a variable
+	 * name.
+	 * 
+	 * @return The internal name of this device.
+	 */
+	public String getDeviceName() {
+		try {
+			return core.getDeviceName(label);
+		} catch (Exception e) {
+			ReportingUtils.logException("Couldn't get device name for " + label, e);
+			return null;
+		}
+	};
+
+	/**
+	 * Sets a property on this device. The exact meaning depends on the property
+	 * (and the device).
+	 * 
+	 * @param property The property name to set.
+	 * @param value The value to set the property to.
+	 */
+	public void setProperty(String property, String value) {
+		try {
+			core.setProperty(label, property, value);
+		} catch (Exception e) {
+			ReportingUtils.logException("Couldn't set \"" + property + "\" to \"" + value + "\" on " + label, e);
+		}
+	}
+
+	/**
+	 * Sets a property on this device (as a double). See above.
+	 * 
+	 * @param property The property name to set.
+	 * @param value The value to set the property to.
+	 */
+	public void setProperty(String property, double value) {
+		try {
+			core.setProperty(label, property, value);
+		} catch (Exception e) {
+			ReportingUtils.logException("Couldn't set \"" + property + "\" to " + value + " on " + label, e);
+		}
+	}
+
+	/**
+	 * Retrieves the value of a property from this device. The exact meaning
+	 * depends on the property and device.
+	 * 
+	 * @param property The property name to get.
+	 * @return A string representation of the property's value.
+	 */
+	public String getProperty(String property) {
+		try {
+			return core.getProperty(label, property);
+		} catch (Exception e) {
+			ReportingUtils.logException("Couldn't get \"" + property + "\" from " + label, e);
+			return null;
+		}
+	}
+
+	/**
+	 * Retrieve the value of a property as a double. See above.
+	 * 
+	 * @param property The property name to get.
+	 * @return The numeric value of the property.
+	 */
+	public double getPropertyDouble(String property) {
+		try {
+			return Double.parseDouble(getProperty(property));
+		} catch (NumberFormatException e) {
+			ReportingUtils.logException("\"" + property + "\" on " + label + " is non-numeric.", e);
+			return Double.NaN; // This is going to mess some stuff up...
+		}
+	}
+
+	/**
+	 * Instruct MM to wait for this device to finish doing something.
+	 */
+	public void waitFor() {
+		try {
+			core.waitForDevice(label);
+		} catch (Exception e) {
+			ReportingUtils.logException("Couldn't wait for " + label, e);
+		}
+	}
+
+	/**
+	 * Get this device's MM label.
+	 * 
+	 * @return The MM label of this device.
+	 */
+	public String getLabel() {
+		return label;
+	};
+
+	/**
+	 * Get this device's MM device type.
+	 * 
+	 * @return The MM type of this device.
+	 */
+	public abstract DeviceType getMMType();
+
+	/*
+	 * Device factory stuff
+	 */
+	public static interface Factory {
+		public abstract spim.setup.Device manufacture(CMMCore core, String label);
+	}
+
+	protected static void installFactory(Factory fact, String name, SPIMDevice... types) {
+		for (SPIMDevice type : types)
+			if(factoryMap.get(type).get(name) == null)
+				factoryMap.get(type).put(name, fact);
+			else
+				throw new Error("Attempt to add second factory for device name \"" + name + "\". Each device name can have only one factory.");
+	}
+
+	protected static Map<SPIMDevice, Map<String, Factory>> factoryMap = new EnumMap<SPIMDevice, Map<String, Factory>>(SPIMDevice.class);
+
+	static {
+		for (SPIMDevice type : SPIMDevice.values())
+			factoryMap.put(type, new HashMap<String, Factory>());
+	}
+
+	public static Device createDevice(CMMCore core, SPIMDevice type, String label) throws Exception {
+		String deviceName = core.getDeviceName(label);
+
+		if (!hasFactory(type, deviceName))
+			return null;
+
+		return factoryMap.get(type).get(deviceName).manufacture(core, label);
+	}
+
+	public static Set<String> getKnownDeviceNames(SPIMDevice type) {
+		return factoryMap.get(type).keySet();
+	}
+
+	public static boolean hasFactory(SPIMDevice type, String deviceName) {
+		return factoryMap.get(type).get(deviceName) != null;
+	}
+}
---
+++ src/main/java/spim/setup/DeviceManager.java
@@ -0,0 +1,96 @@
+package spim.setup;
+
+import java.awt.GridLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Arrays;
+import java.util.EventListener;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Vector;
+
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.micromanager.utils.ReportingUtils;
+
+import spim.LayoutUtils;
+import spim.setup.SPIMSetup.SPIMDevice;
+
+public class DeviceManager extends JPanel implements ItemListener, EventListener {
+	private SPIMSetup setup;
+	private JFrame display;
+
+	public DeviceManager(SPIMSetup setup) {
+		this.setup = setup;
+
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+		JPanel stages = LayoutUtils.titled("Stages", new JPanel(new GridLayout(4, 2, 0, 2)));
+		LayoutUtils.addAll(stages, labelCombo(SPIMDevice.STAGE_X));
+		LayoutUtils.addAll(stages, labelCombo(SPIMDevice.STAGE_Y));
+		LayoutUtils.addAll(stages, labelCombo(SPIMDevice.STAGE_Z));
+		LayoutUtils.addAll(stages, labelCombo(SPIMDevice.STAGE_THETA));
+		add(stages);
+
+		JPanel illum = LayoutUtils.titled("Illumination/Detection", new JPanel(new GridLayout(5, 2, 0, 2)));
+		LayoutUtils.addAll(illum, labelCombo(SPIMDevice.LASER1));
+		LayoutUtils.addAll(illum, labelCombo(SPIMDevice.LASER2));
+		LayoutUtils.addAll(illum, labelCombo(SPIMDevice.CAMERA1));
+		LayoutUtils.addAll(illum, labelCombo(SPIMDevice.CAMERA2));
+		LayoutUtils.addAll(illum, labelCombo(SPIMDevice.SYNCHRONIZER));
+		add(illum);
+	}
+
+	public void setVisible(boolean show) {
+		if (display == null) {
+			display = new JFrame("Device Manager");
+			display.add(this);
+			display.pack();
+		}
+
+		display.setVisible(show);
+	}
+
+	private JComponent[] labelCombo(SPIMDevice type) {
+		Vector<String> devices = new Vector<String>(Arrays.asList(setup.getCore().getLoadedDevices().toArray()));
+		Set<String> names = Device.getKnownDeviceNames(type);
+
+		Iterator<String> iter = devices.iterator();
+		while (iter.hasNext())
+			try {
+				if (!names.contains(setup.getCore().getDeviceName(iter.next())))
+					iter.remove();
+			} catch (Throwable t) {
+				ReportingUtils.logError(t);
+			}
+
+		devices.add("(none)");
+
+		JComboBox combo = new JComboBox(devices);
+		combo.setName(type.toString());
+		combo.setSelectedItem(setup.getDevice(type) != null ? setup.getDevice(type).getLabel() : "(none)");
+		combo.addItemListener(this);
+
+		return new JComponent[] { new JLabel(type.getText()), combo };
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent ie) {
+		if (!(ie.getSource() instanceof JComboBox))
+			return; // what
+
+		JComboBox src = (JComboBox) ie.getSource();
+		SPIMDevice type = SPIMDevice.valueOf(src.getName());
+		String selectedLabel = src.getSelectedItem().toString();
+		
+		if(selectedLabel.equals("(none)"))
+			selectedLabel = null;
+
+		setup.setDevice(type, selectedLabel);
+	}
+}
