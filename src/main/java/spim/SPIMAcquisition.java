b82050c9253cba589656dd173c966c2d80b6b682
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2013-08-29 14:37:43 -0500

    Added LayoutUtils.form and made use of it
    Greatly simplifies certain types of UI creation.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- src/main/java/spim/SPIMAcquisition.java
+++ src/main/java/spim/SPIMAcquisition.java
@@ -143,7 +143,7 @@ public class SPIMAcquisition implements MMPlugin, MouseMotionListener, KeyListen
 	protected JCheckBox liveCheckbox, registrationCheckbox, continuousCheckbox;
 	protected JButton speedControl, ohSnap;
 	protected JFrame acqOptionsFrame;
-	protected JCheckBox asyncMonitorCheckbox;
+	protected JCheckBox asyncMonitorCheckbox, acqProfileCheckbox;
 
 	protected boolean updateLiveImage;
 
@@ -928,13 +928,14 @@ public class SPIMAcquisition implements MMPlugin, MouseMotionListener, KeyListen
 		acqProfileCheckbox = new JCheckBox(/*"Profile Acquisition"*/);
 
 		acqOptionsFrame = new JFrame("Acquisition Options");
-		JPanel optsPanel = new JPanel();
-		optsPanel.setLayout(new GridLayout(4, 2));
+		JPanel optsPanel = LayoutUtils.form(
+				"Z settle time (ms):", settleTime,
+				"Continuous Mode:", continuousCheckbox,
+				"SPIM Registration:", registrationCheckbox,
+				"Monitor Async Output:", asyncMonitorCheckbox,
+				"Profile Acquisition:", acqProfileCheckbox
+		);
 		optsPanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
-		optsPanel.add(rightAlign("Continuous Mode:")); optsPanel.add(continuousCheckbox);
-		optsPanel.add(rightAlign("SPIM Registration:")); optsPanel.add(registrationCheckbox);
-		optsPanel.add(rightAlign("Monitor Async Output:")); optsPanel.add(asyncMonitorCheckbox);
-		optsPanel.add(rightAlign("Z settle time (ms):")); optsPanel.add(LayoutUtils.horizPanel(settleTime, Box.createHorizontalGlue()));
 		acqOptionsFrame.add(optsPanel);
 		acqOptionsFrame.pack();
 
@@ -1312,10 +1313,6 @@ public class SPIMAcquisition implements MMPlugin, MouseMotionListener, KeyListen
 	public void keyTyped(KeyEvent ke) {}
 
 	// UI helpers
-
-	protected static Component rightAlign(String text) {
-		return LayoutUtils.horizPanel(Box.createHorizontalGlue(), new JLabel(text));
-	}
 
 	protected enum Justification {
 		LEFT, STRETCH, RIGHT
