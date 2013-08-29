312da30e0acd041738cb74c15b07df4f65fee804
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2013-08-29 14:37:43 -0500

    Added LayoutUtils.form and made use of it
    Greatly simplifies certain types of UI creation.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- src/main/java/spim/LayoutUtils.java
+++ src/main/java/spim/LayoutUtils.java
@@ -1,8 +1,13 @@
 package spim;
 
 import java.awt.Component;
+import java.awt.Container;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,42 +19,93 @@ public class LayoutUtils {
 		return c;
 	}
 
-	public static Component vertPanel(Component... parts) {
+	public static JPanel vertPanel(Component... parts) {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 		
 		return addAll(panel, parts);
 	}
 
-	public static Component horizPanel(Component... parts) {
+	public static JPanel horizPanel(Component... parts) {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
 		
 		return addAll(panel, parts);
 	}
 
-	public static Component vertPanel(String title, Component... parts) {
+	public static JPanel vertPanel(String title, Component... parts) {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 		
 		return addAll(titled(title, panel), parts);
 	}
 
-	public static Component horizPanel(String title, Component... parts) {
+	public static JPanel horizPanel(String title, Component... parts) {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
 		
 		return addAll(titled(title, panel), parts);
 	};
 
-	public static JComponent addAll(JComponent to, Component... parts) {
+	public static <T extends Container> T addAll(T to, Component... parts) {
 		for(Component c : parts)
 			to.add(c);
 
 		return to;
 	}
 
-	public static Component labelMe(Component c, String lbl) {
+	public static JPanel labelMe(Component c, String lbl) {
 		return horizPanel(new JLabel(lbl), c);
+	}
+
+	public static JPanel form(Map<String, Component> labelCompPairs) {
+		JPanel out = new JPanel();
+		GroupLayout layout = new GroupLayout(out);
+		out.setLayout(layout);
+
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+
+		GroupLayout.Group horizontalLabelsGroup = layout.createParallelGroup(GroupLayout.Alignment.TRAILING);
+		GroupLayout.Group horizontalComponentsGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+		GroupLayout.Group verticalGroup = layout.createSequentialGroup();
+
+		for(Map.Entry<String, Component> formEntry : labelCompPairs.entrySet()) {
+			JLabel label = new JLabel(formEntry.getKey());
+
+			horizontalLabelsGroup.addComponent(label);
+			horizontalComponentsGroup.addComponent(formEntry.getValue());
+
+			verticalGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+					.addComponent(label)
+					.addComponent(formEntry.getValue())
+			);
+		}
+
+		layout.setVerticalGroup(verticalGroup);
+		layout.setHorizontalGroup(layout.createSequentialGroup()
+				.addGroup(horizontalLabelsGroup)
+				.addGroup(horizontalComponentsGroup)
+		);
+
+		return out;
+	}
+
+	public static JPanel form(Object... labelCompPairs) {
+		if(labelCompPairs.length % 2 != 0)
+			throw new IllegalArgumentException("label/component count must be equal");
+
+		Map<String, Component> labelCompMap = new LinkedHashMap<String, Component>();
+
+		for(int i=0; i < labelCompPairs.length; i += 2) {
+			if(!(labelCompPairs[i] instanceof String))
+				throw new IllegalArgumentException("argument " + i + " not a string: " + labelCompPairs[i].toString());
+			if(!(labelCompPairs[i+1] instanceof Component))
+				throw new IllegalArgumentException("argument " + (i+1) + " not a component: " + labelCompPairs[i+1].toString());
+
+			labelCompMap.put(labelCompPairs[i].toString(), (Component) labelCompPairs[i+1]);
+		}
+
+		return form(labelCompMap);
 	}
 }
