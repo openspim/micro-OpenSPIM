4a53d1eeaee84f657facab2c3d63ae8b20aae7de
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2013-09-05 14:36:05 -0500

    Removed pixel size component from SPIMCalibrator
    The SPIMCalibrator interface is now solely for determining the axis of
    rotation. Before long it will likely be de-interfaced in favor of solely
    the autocalibrator.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- src/main/java/spim/SPIMCalibrator.java
+++ src/main/java/spim/SPIMCalibrator.java
@@ -4,8 +4,6 @@ import org.apache.commons.math.geometry.euclidean.threed.Vector3D;
 
 public interface SPIMCalibrator {
 
-	public abstract double getUmPerPixel();
-
 	public abstract Vector3D getRotationOrigin();
 
 	public abstract Vector3D getRotationAxis();
