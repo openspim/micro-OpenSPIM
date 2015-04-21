17aec48c78871d215623ec1997db475f4ea6a0a0
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-04-21 17:01:20 +0200

    Add a script to deploy the Micro-Manager artifacts via Jenkins
    This script is intended to be executed by
    
    	http://jenkins.imagej.net/view/OpenSPIM/job/Deploy-Micro-Manager-Jars/
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ scripts/jenkins-deploy-jars.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+curl -O http://update.imagej.net/bootstrap.js
+
+test -f db.xml.gz ||
+jrunscript bootstrap.js add-update-site Micro-Manager-dev http://sites.imagej.net/Micro-Manager-dev
+
+now=$(date +%s)
+jrunscript bootstrap.js update plugins/Micro-Manager/MMCoreJ.jar plugins/Micro-Manager/MMJ_.jar
+
+for artifactId in MMCoreJ MMJ_
+do
+	file="$(pwd)/plugins/Micro-Manager/${artifactId}.jar"
+	if test $(stat -c %Y "$file") -ge $now
+	then
+		# unfortunately, this version is not recorded on the update site
+		version=1.4.21-SNAPSHOT
+		mvn -f "$(dirname "$0")/../pom.xml" -Pdeploy-to-imagej deploy:deploy-file \
+			-Durl=dav:http://maven.imagej.net/content/repositories/snapshots \
+			-DrepositoryId=imagej.snapshots \
+			-DgroupId=org.micromanager \
+			-Dversion=$version \
+			-Dpackaging=jar \
+			-DartifactId=${artifactId} \
+			-Dfile="$file"
+	fi
+done
