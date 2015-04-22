1e48143775ad13fef4ca4a2cda96dc3dbe084068
HongKee Moon <hkmoon@me.com>
2015-04-22 10:45:29 +0200

    Add SPIMTestDrive for launching "mm"

---
+++ src/test/java/SPIMTestDrive.java
@@ -0,0 +1,12 @@
+/**
+ * Created by moon on 4/20/15.
+ */
+public class SPIMTestDrive
+{
+	public static void main(String[] args)
+	{
+		MMStudioPlugin mm = new MMStudioPlugin();
+
+		mm.run( "" );
+	}
+}
