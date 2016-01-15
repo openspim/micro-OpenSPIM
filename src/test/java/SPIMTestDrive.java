664e1420134b4b9b203e9feb2de0da85eb8db19c
HongKee Moon <hkmoon@me.com>
2016-01-15 15:37:20 +0100

    Support two ways of testing SPIMAcquisition plugin
    SPIMTestDrive is used for testing either only with micro-manager or with micro-manager within ImageJ instance.

--- src/test/java/SPIMTestDrive.java
+++ src/test/java/SPIMTestDrive.java
@@ -1,12 +1,25 @@
 import java.io.File;
+
+import ij.IJ;
+import ij.ImageJ;
+import org.micromanager.MMStudio;
+import org.micromanager.utils.JavaUtils;
+import org.micromanager.utils.ReportingUtils;
 import org.scijava.util.ClassUtils;
 import spim.SPIMAcquisition;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 /**
- * Created by moon on 4/20/15.
+ * SPIMTestDrive launches MMStudio with the current SPIMAcquisitionPlugin
+ * Note: set the working folder to where Micro-Manager1.4 is located
  */
 public class SPIMTestDrive
 {
+	static MMStudio studio_;
+
 	public static void main(String[] args)
 	{
 		final File classesDirectory = new File(ClassUtils.getLocation( SPIMAcquisition.class.getName() ).getPath());
@@ -15,8 +28,56 @@ public class SPIMTestDrive
 
 		System.err.println(System.getProperty("org.micromanager.plugin.path"));
 
-		MMStudioPlugin mm = new MMStudioPlugin();
+		// Choose either openMMStudioInImageJ() or openMMStudioOnly()
 
-		mm.run( "" );
+		//openMMStudioInImageJ()
+		openMMStudioOnly();
+	}
+
+	// When users want to test the mmplugin with ImageJ instance
+	public static void openMMStudioInImageJ()
+	{
+		// This way would work with IJ directory together
+		new ImageJ();
+		IJ.getInstance().exitWhenQuitting( true );
+	}
+
+	// When users want to test only the mmplugin without ImageJ instance
+	public static void openMMStudioOnly()
+	{
+		SwingUtilities.invokeLater( new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					if ( studio_ == null || !studio_.getIsProgramRunning() )
+					{
+						// OS-specific stuff
+						if ( JavaUtils.isMac() )
+						{
+							System.setProperty( "apple.laf.useScreenMenuBar", "true" );
+						}
+						try
+						{
+							UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
+						}
+						catch ( Exception e )
+						{
+							ReportingUtils.logError( e );
+						}
+
+						studio_ = new MMStudio( false );
+						MMStudio.getFrame().setVisible( true );
+						MMStudio.getFrame().setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+					}
+				}
+				catch ( Exception e )
+				{
+					ReportingUtils.logError( e );
+				}
+			}
+		} );
 	}
 }
