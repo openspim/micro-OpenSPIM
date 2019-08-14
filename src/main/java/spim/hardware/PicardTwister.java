a3057c60e8d8ed5a4f1e6a5ae3c6f564128c6d24
HongKee Moon <hkmoon@me.com>
2019-08-14 16:22:41 +0200

    MM does the conversion with the initial device property

--- src/main/java/spim/hardware/PicardTwister.java
+++ src/main/java/spim/hardware/PicardTwister.java
@@ -15,39 +15,4 @@ public class PicardTwister extends GenericRotator {
 	public PicardTwister(CMMCore core, String label) {
 		super(core, label);
 	}
-
-	@Override
-	public double getStepSize() {
-		return 1.8; // degrees/step
-	}
-
-	@Override
-	public double getPosition() {
-		double currentPosition = stepsToDegrees(super.getPosition());
-
-//		if (Math.abs(currentPosition) <= 180) // TODO: If wrap-around is really important, these should be uncommented and fixed.
-			return currentPosition;
-/*		else if (currentPosition > 180) // Alternatively, this code might be moved to the Picard DAL -- almost makes more sense there.
-			return ((currentPosition - 180) % 360) - 180;
-		else
-			return ((currentPosition + 180) % 360) + 180;*/
-	}
-
-	@Override
-	public void setPosition(double pos) {
-		double currentPosition = getPosition();
-
-//		if (Math.abs(pos - currentPosition) <= 180.0)
-			super.setPosition(super.getPosition() + degreesToSteps(pos - currentPosition));
-/*		else
-			super.setPosition(super.getPosition() + degreesToSteps(pos - currentPosition - 360*Math.signum(pos - currentPosition)));*/
-	}
-
-	private double stepsToDegrees(double steps) {
-		return steps * 180.0D / 100.0D;
-	}
-
-	private double degreesToSteps(double degrees) {
-		return degrees * 100.0D / 180.0D;
-	}
 }
