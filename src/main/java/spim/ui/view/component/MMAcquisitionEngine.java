6e4c555fe785de894c3779410c364a4d70ad347c
HongKee Moon <hkmoon@me.com>
2020-05-14 21:53:48 +0200

    Fix for the multi camera in arduino

--- src/main/java/spim/ui/view/component/MMAcquisitionEngine.java
+++ src/main/java/spim/ui/view/component/MMAcquisitionEngine.java
@@ -159,7 +159,8 @@ public class MMAcquisitionEngine
 		int ch = 0;
 		if(arduinoSelected) {
 			cameras.add(currentCamera);
-			ch++;
+			if(currentCamera.startsWith( "Multi" )) ch += 2;
+			else ch += 1;
 		} else {
 			for(ChannelItem chItem: channelItems) {
 				if(!cameras.contains( chItem.getName() )) {
