4821d957c5d828ce47e68b4f3e85f274abaca883
HongKee Moon <moon@mpi-cbg.de>
2021-07-02 14:22:05 +0200

    Supported binning of PCOCamera

--- src/main/java/spim/hardware/PCOCamera.java
+++ src/main/java/spim/hardware/PCOCamera.java
@@ -17,6 +17,24 @@ public class PCOCamera extends Camera {
 		}, "PCO Camera", SPIMSetup.SPIMDevice.CAMERA1, SPIMSetup.SPIMDevice.CAMERA2);
 	}
 
+	@Override
+	public void setBinning(String val) {
+		if (val.contains("x")) {
+			super.setBinning(val);
+		} else {
+			String str = "1";
+			switch (val) {
+				case "1" : str = "1x1"; break;
+				case "2" : str = "2x2"; break;
+				case "3" : str = "3x3"; break;
+				case "4" : str = "4x4"; break;
+				case "8" : str = "8x8"; break;
+			}
+
+			super.setBinning(str);
+		}
+	}
+
 	public PCOCamera(CMMCore core, String label) {
 		super(core, label);
 	}
