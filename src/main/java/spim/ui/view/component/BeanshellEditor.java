01d712509339246bd5d9f5b7316a59d052585e68
HongKee Moon <moon@mpi-cbg.de>
2021-08-30 16:57:42 +0200

    Removed unused imports

--- src/main/java/spim/ui/view/component/BeanshellEditor.java
+++ src/main/java/spim/ui/view/component/BeanshellEditor.java
@@ -9,12 +9,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
 import javafx.scene.layout.HBox;
 
-import javafx.stage.FileChooser;
 import org.micromanager.Studio;
 import org.micromanager.internal.utils.MMScriptException;
 import org.micromanager.internal.utils.ReportingUtils;
--- src/main/java/spim/ui/view/component/JavaEditor.java
+++ src/main/java/spim/ui/view/component/JavaEditor.java
@@ -5,7 +5,6 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.DragEvent;
 import javafx.scene.layout.HBox;
 
 import mmcorej.TaggedImage;
