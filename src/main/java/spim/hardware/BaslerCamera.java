bcecf19570c2d7906a5ea153b0a2ea909ee7d659
HongKee Moon <moon@mpi-cbg.de>
2023-06-05 15:55:02 +0200

    feat: support BaslerCamera

--- src/main/java/spim/hardware/BaslerCamera.java
+++ src/main/java/spim/hardware/BaslerCamera.java
@@ -5,19 +5,19 @@ import mmcorej.CMMCore;
 /**
  * Author: HongKee Moon (moon@mpi-cbg.de), Scientific Computing Facility
  * Organization: MPI-CBG Dresden
- * Date: May 2023
+ * Date: June 2023
  */
-public class TSICam extends Camera {
+public class BaslerCamera extends Camera {
 	static {
 		Device.installFactory(new Device.Factory() {
 			@Override
 			public Device manufacture(CMMCore core, String label ) {
-				return new TSICam(core, label);
+				return new BaslerCamera(core, label);
 			}
-		}, "TSICam", SPIMSetup.SPIMDevice.CAMERA1, SPIMSetup.SPIMDevice.CAMERA2);
+		}, "BaslerCamera", SPIMSetup.SPIMDevice.CAMERA1, SPIMSetup.SPIMDevice.CAMERA2);
 	}
 
-	public TSICam(CMMCore core, String label) {
+	public BaslerCamera(CMMCore core, String label) {
 		super(core, label);
 	}
 }
