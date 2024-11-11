63296140c1975838859cb1bf19e827bcbd3f5aa7
HongKee Moon <moon@mpi-cbg.de>
2024-11-11 15:50:30 +0100

    chore: include MMAcqEngine

--- scripts/installMMDeps.sh
+++ scripts/installMMDeps.sh
@@ -12,4 +12,9 @@ mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
                          -DartifactId=MMJ_ -Dversion=2.0.0 \
                          -Dpackaging=jar -DlocalRepositoryPath=local-repo/
 
+mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file \
+                         -Dfile=lib/MMAcqEngine.jar -DgroupId=org.micromanager \
+                         -DartifactId=MMAcqEngine -Dversion=2.0.0 \
+                         -Dpackaging=jar -DlocalRepositoryPath=local-repo/
+
 mvn install:install-file -DgroupId=jdk.tools -DartifactId=jdk.tools -Dpackaging=jar -Dversion=1.8 -Dfile=lib/tools.jar -DgeneratePom=true
